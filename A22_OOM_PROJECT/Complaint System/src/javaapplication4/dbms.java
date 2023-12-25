48bd53622841104ef5733f0f8ca9a9575c364d8a
Hannah2k23 <129401594+Hannah2k23@users.noreply.github.com>
2023-12-25 13:31:45 +0530

    Add files via upload

---
+++ A22_OOM_PROJECT/Complaint System/src/javaapplication4/dbms.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication4;
+import java.util.ArrayList;
+/**
+ *
+ * @author Prernendu Bhagat
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class dbms {
+    //public ArrayList<String> sname = new ArrayList<String>();
+    static ArrayList<String> sname = new ArrayList<String>(Arrays.asList("Priyal","Mansa","Bhagat","Amit"));
+    static ArrayList<String> id = new ArrayList<String>(Arrays.asList("076@iiita.ac.in", "017@iiita.ac.in","016@iiita.ac.in","034@iiita.ac.in"));
+    static ArrayList<String> pswd = new ArrayList<String>(Arrays.asList("123","456","789","000"));
+
+    static ArrayList<String> admin_name = new ArrayList<String>(Arrays.asList("Admin1"));
+    static ArrayList<String> admin_id = new ArrayList<String>(Arrays.asList("Admin1@bennett.edu.in"));
+    static ArrayList<String> admin_pswd = new ArrayList<String>(Arrays.asList("Admin1"));
+
+    static ArrayList<Integer> comp_no = new ArrayList<Integer>();
+    static ArrayList<String> dscrptn = new ArrayList<String>();
+    static ArrayList<String> status = new ArrayList<String>();
+
+//    public void login_cred(String s_id, String s_pswd){
+//        if(Arrays.asList(id).contains(s_id)){
+//            int n = id.indexOf(s_id);
+//            if(pswd.get(n).equals(s_pswd)){
+//
+//            }
+//        }
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(id);
+//    }
+}
---
+++ A22_OOM_PROJECT/ReadME.txt
@@ -0,0 +1,19 @@
+this is java project using swing and AWT.. It takes complains from users and allows an Admin to access the complaint and resolve it. User and Admin have to login to access the system.
+
+//Procedure
+
+install apache netbeans...
+open the project file named Complaint System..
+open the file with the name JavaApplication4.java in src..
+cick on the Run Button or press f6
+Login details for the user are : ID: 016@iiita.ac.in   Password: 789
+				 ID: 017@iiita.ac.in   Password: 456
+				 ID: 034@iiita.ac.in   Password: 000
+				 ID: 076@iiita.ac.in   Password: 123
+
+Login details for the Admin is:  ID: admin@iiita.ac.in Password: Admin
+
+Login with the user to lodge and view contents, login with the Admin to resolve complaints and add new users.
+
+
+
